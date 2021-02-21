012f556aba68621d1991a479414002f4484ced25
bombin78 <bombin78@mail.ru>
2021-02-22 00:06:06 +0300

    Hello world from JS

---
+++ index.js
@@ -0,0 +1,56 @@
+const model = [
+    {type: 'title', value: 'Hello world from JS'},
+    {type: 'text', value: 'here we go with some text'},
+    {type: 'columns', value: [
+            '111111111',
+            '222222222',
+            '333333333',
+        ],
+    },
+];
+
+const $site = document.querySelector('#site');
+
+model.forEach(block => {
+
+    let html = '';
+
+    if (block.type === 'title') {
+        html = title(block);
+    } else if (block.type === 'text') {
+        html = text(block);
+    } else if (block.type === 'columns') {
+        html = columns(block);
+    }
+
+    $site.insertAdjacentHTML('beforeend', html);
+})
+
+function title(block) {
+    return `
+        <div class="row">
+            <div class="col-sm">
+                <h1>${block.value}</h1>
+            </div>
+        </div>
+    `;
+}
+
+function text(block) {
+    return `
+        <div class="row">
+            <div class="col-sm">
+                <p>${block.value}</p>
+            </div>
+        </div>
+    `;
+}
+
+function columns(block) {
+    const html = block.value.map(item => `<div class="col-sm"><p>${item}</p></div>`);
+    return `
+        <div class="row">
+            ${html.join('')}
+        </div>
+    `;
+}
