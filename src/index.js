3d3c06ec5ee5543fd6719651e1601d4842c1a32f
bombin78 <bombin78@mail.ru>
2021-02-24 22:48:20 +0300

    рефакторинг

--- src/index.js
+++ src/index.js
@@ -1,24 +1,13 @@
 import {model} from '@/model';
-import {title, text, columns, image} from '@/templates'; 
-import '@/babel';
+import {templates} from '@/templates'; 
 import '@/styles/main.css';
 import '@/styles/scss.scss';
 
 const $site = document.querySelector('#site');
 
 model.forEach(block => {
-
-    let html = '';
-
-    if (block.type === 'title') {
-        html = title(block);
-    } else if (block.type === 'text') {
-        html = text(block);
-    } else if (block.type === 'columns') {
-        html = columns(block);
-    } else if (block.type === 'image') {
-        html = image(block);
+    const toHtml = templates[block.type];
+    if(toHtml) {
+        $site.insertAdjacentHTML('beforeend', toHtml(block));
     }
-
-    $site.insertAdjacentHTML('beforeend', html);
 });
