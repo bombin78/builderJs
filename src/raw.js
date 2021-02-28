af51be9095b018fbd34366bf6ea9fdca44dba4a0
bombin78 <bombin78@mail.ru>
2021-03-01 00:19:55 +0300

    Модальные окна, карточки, видеоконтент: верстка + добавлена переменная

---
+++ src/raw.js
@@ -0,0 +1,19 @@
+import * as $ from 'jquery';
+
+// Настройка формы авторизаации
+// $('#exampleModal').modal({
+//     keyboard: false,
+//     backdrop: false,
+// });
+
+// Валидация формы авторизаации
+const forms = document.getElementsByClassName('needs-validation');
+const validation = Array.prototype.filter.call(forms, function(form) {
+    form.addEventListener('submit', function(event) {
+    if (form.checkValidity() === false) {
+        event.preventDefault();
+        event.stopPropagation();
+    }
+    form.classList.add('was-validated');
+    }, false);
+});
--- webpack.config.js
+++ webpack.config.js
@@ -52,7 +52,7 @@ module.exports = {
     context: path.resolve(__dirname, 'src'),
     mode: 'development',
     entry: {
-        main: ['@babel/polyfill', './index.js'],
+        main: ['@babel/polyfill', './index.js', './raw.js'],
         other: './other.ts',
     },
     output: {
